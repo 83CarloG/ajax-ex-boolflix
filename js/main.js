9bca1418463e3a4000ac252e0efebcc4671906ff
Carlo Giuseppe Colombo <cgcolombo1983@hotmail.com>
2020-09-26 16:21:13 +0200

    fix css milestone 4

--- js/main.js
+++ js/main.js
@@ -24,6 +24,20 @@
 	Esempio di URL che torna la copertina di PEPPA PIG:
 	https://image.tmdb.org/t/p/w185/tZgQ76hS9RFIwFyUxzctp1Pkz0N.jpg
 */
+/*
+#MILESTONE 4:
+	Trasformiamo quello che abbiamo fatto fino ad ora in una vera e propria webapp, creando un layout completo simil-Netflix:
+	Un header che contiene logo e search bar
+	Dopo aver ricercato qualcosa nella searchbar, i risultati appaiono sotto forma di “card” in cui lo sfondo è rappresentato dall’immagine di copertina (consiglio la poster_path con w342)
+	Andando con il mouse sopra una card (on hover), appaiono le informazioni aggiuntive già prese nei punti precedenti più la overview
+*/
+/*
+#MILESTONE 5-6
+	Milestone :5: (Opzionale):
+	Partendo da un film o da una serie, richiedere all'API quali sono gli attori che fanno parte del cast aggiungendo alla nostra scheda Film / Serie SOLO i primi 5 restituiti dall’API con Nome e Cognome, e i generi associati al film con questo schema: “Genere 1, Genere 2, …”. :dancer:
+	Milestone :6: (Opzionale):
+	Creare una lista di generi richiedendo quelli disponibili all'API e creare dei filtri con i generi tv e movie per mostrare/nascondere le schede ottenute con la ricerca
+*/
 $(document).ready(function	() {
 	const boolflix = {
 		serchClick: serchClick('.serch__btn', '.serch__input'),
@@ -50,8 +64,10 @@ function render	(input, type) {
 		var html = template(context);
 		// Inseriamo il template nell' id #list-movies o #list-series
 		if (type === 'movie') {
+			$('.type-movies').removeClass('d-none');
 			$('.list-movies').append(html);
 		} else {
+			$('.type-series').removeClass('d-none');
 			$('.list-series').append(html);
 		}
 	}
